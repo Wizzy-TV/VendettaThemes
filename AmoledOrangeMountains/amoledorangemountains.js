ba81137d00238929df24ff6d97ff42c7ed8759d4
Wizzy <82947033+Wizzy-TV@users.noreply.github.com>
2023-04-18 16:11:00 +0200

    Create amoledorangemountains.js

---
+++ AmoledOrangeMountains/amoledorangemountains.js
@@ -0,0 +1,131 @@
+{
+    "name": "Orange Mountains",
+    "description": "A simple theme for Vendetta with an image of orange mountains. DARK MODE REQUIRED!",
+    "authors": [
+        {
+            "name": "Wizzy#0211",
+            "id": "715443625580036128"
+        }
+    ],
+    "semanticColors": {
+        "HEADER_PRIMARY": [
+            "#ffffff"
+        ],
+        "HEADER_SECONDARY": [
+            "#ed6a19"
+        ],
+        "TEXT_NORMAL": [
+            "#ffffff"
+        ],
+        "TEXT_MUTED": [
+            "#ed6a19"
+        ],
+        "TEXT_LINK": [
+            "#ed6a19"
+        ],
+        "TEXT_DANGER": [
+            "#ed6a19"
+        ],
+        "INTERACTIVE_NORMAL": [
+            "#ffffff"
+        ],
+        "INTERACTIVE_ACTIVE": [
+            "#ffffff"
+        ],
+        "INTERACTIVE_MUTED": [
+            "#808080"
+        ],
+        "BACKGROUND_PRIMARY": [
+            "#181818"
+        ],
+        "BACKGROUND_SECONDARY": [
+            "#181818"
+        ],
+        "BACKGROUND_SECONDARY_ALT": [
+            "#181818"
+        ],
+        "BACKGROUND_TERTIARY": [
+            "#181818"
+        ],
+        "BACKGROUND_ACCENT": [
+            "#ed6a19"
+        ],
+        "BACKGROUND_FLOATING": [
+            "#181818"
+        ],
+        "BACKGROUND_MOBILE_PRIMARY": [
+            "#181818"
+        ],
+        "BACKGROUND_MOBILE_SECONDARY": [
+            "#181818"
+        ],
+        "BACKGROUND_NESTED_FLOATING": [
+            "#181818"
+        ],
+        "BACKGROUND_MENTIONED": [
+            "#e6ed6a19"
+        ],
+        "BACKGROUND_MODIFIER_ACTIVE": [
+            "#181818"
+        ],
+        "BACKGROUND_MODIFIER_SELECTED": [
+            "#ed6a19"
+        ],
+        "BACKGROUND_MODIFIER_ACCENT": [
+            "#181818"
+        ],
+        "SPOILER_HIDDEN_BACKGROUND": [
+            "#181818"
+        ],
+        "SPOILER_REVEALED_BACKGROUND": [
+            "transparent"
+        ],
+        "CHANNEL_TEXT_AREA_PLACEHOLDER": [
+            "#999999"
+        ],
+        "CHAT_BACKGROUND": [
+            "#181818"
+        ],
+        "CHANNELS_DEFAULT": [
+            "#c4c4c4"
+        ],
+        "TEXT_LINK": [
+            "#ed6a19"
+        ],
+        "CHANNEL_ICON": [
+            "#ffffff"
+        ]
+    },
+    "rawColors": {
+        "PRIMARY_100": "#ffffff",
+        "PRIMARY_200": "#ffffff",
+        "PRIMARY_300": "#ffffff",
+        "PRIMARY_360": "#ed6a19",
+        "PRIMARY_500": "#ed6a19",
+        "PRIMARY_600": "#181818",
+        "PRIMARY_630": "#181818",
+        "PRIMARY_660": "#181818",
+        "PRIMARY_700": "#181818",
+        "PRIMARY_800": "#181818",
+        "BRAND_260": "#ed6a19",
+        "BRAND_360": "#ed6a19",
+        "BRAND_500": "#ed6a19",
+        "BRAND_560": "#ffffff",
+        "WHITE_500": "#ffffff",
+        "BLUE_345": "#ed6a19",
+        "YELLOW_300": "#ed6a19",
+        "ORANGE_345": "#ed6a19",
+        "GREEN_345": "#ed6a19",
+        "GREEN_360": "#ed6a19",
+        "RED_560" : "#ed6a19",
+        "RED_400": "#ed6a19"
+    },
+    "spec": 2,
+    "background": {
+        "blur": 0.6,
+        "url": "https://raw.githubusercontent.com/Wizzy-TV/VendettaThemes/main/OrangeMountains/wallpaper.jpg",
+        "alpha": 0.5,
+        "color": "#181818"
+    }
+}
+
